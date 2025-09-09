37dc4536785af24a190ac59f0787e817468c5405
Bona Philippe <bona.lukengu@deimos.co.za>
2025-09-09 03:15:38 +0200

    origin

--- app/hairitage/settings.py
+++ app/hairitage/settings.py
@@ -145,4 +145,4 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 JAZZMIN_SETTINGS = JAZZMIN_SETTINGS
-CSRF_TRUSTED_ORIGINS = ["http://localhost:1337"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:1337, https://hairitage-studio.co.za"]
