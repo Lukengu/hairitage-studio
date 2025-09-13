b3d0890bf6d650f37dde6424888372e8b8ce6e28
Bona Philippe <bona.lukengu@deimos.co.za>
2025-09-13 06:40:56 +0200

    blog

--- app/blog/views.py
+++ app/blog/views.py
@@ -9,7 +9,7 @@ def blog_details(request):
     post = Post.objects.get(pk=id)
     categories = Category.objects.raw("""
         SELECT blog_category.id, name, COUNT(blog_post.*) as ct FROM blog_category LEFT JOIN blog_post
-        ON blog_category.id = blog_post.category_id_id   GROUP BY blog_category.id ORDER BY blog_category.name
+        ON blog_category.id = blog_post.category_id_id WHERE type = 'blog'   GROUP BY blog_category.id ORDER BY blog_category.name
     """)
     return render(request, 'site/blog/details.html', {'post': post, 'categories': categories})
 
