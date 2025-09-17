ebeb104ec7b9d621cc1c7b5edb5308e314b66bf6
Bona Philippe <bona.lukengu@deimos.co.za>
2025-09-17 22:33:41 +0200

    import

--- app/log/urls.py
+++ app/log/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .log.views import whatsapp_webhook
+from .views import whatsapp_webhook
 
 urlpatterns = [
     path('whatsapp/webhook', whatsapp_webhook, name="whatsapp_webhook")
